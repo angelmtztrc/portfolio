833f2c7819ef0e319143889bdd7671c912bc8ca7
Angel Martinez <angel.codes139@gmail.com>
2020-09-08 20:07:55 -0500

    starting with contact page

---
+++ pages/contact.js
@@ -0,0 +1,26 @@
+import 'twin.macro';
+
+// Components
+import Layout from '../components/Layout/Layout';
+import Content from '../components/ui/Content';
+import Heading from '../components/ui/Heading';
+
+const Portfolio = () => {
+    return (
+        <Layout>
+            <Content>
+                <div>
+                    <Heading>Contact</Heading>
+                    <p tw="text-base mb-5">
+                        Do you need my skills? Contact me to give you more
+                        information about what I do, and in this way to be able
+                        to provide you with a specific solution for your needs.
+                    </p>
+                </div>
+                {/* TODO : Form for send emails */}
+            </Content>
+        </Layout>
+    );
+};
+
+export default Portfolio;
