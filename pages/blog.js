675a15d59124851765632687a4460dda1f485c19
Angel Martinez <angel.codes139@gmail.com>
2020-09-08 19:48:27 -0500

    starting with blog page

---
+++ pages/blog.js
@@ -0,0 +1,24 @@
+import 'twin.macro';
+
+// Components
+import Layout from '../components/Layout/Layout';
+import Content from '../components/ui/Content';
+import Heading from '../components/ui/Heading';
+
+const Blog = () => {
+    return (
+        <Layout>
+            <Content>
+                <div>
+                    <Heading>Blog</Heading>
+                    <p tw="text-base mb-5">
+                        Here you can see some blog posts about tutorials, tips,
+                        or technologies that I have written.
+                    </p>
+                </div>
+            </Content>
+        </Layout>
+    );
+};
+
+export default Blog;
