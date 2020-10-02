4e5d742b03661280b3004c39ee55b2693d1a29f9
Angel Martinez <65056245+angel-codes@users.noreply.github.com>
2020-10-02 13:12:20 -0500

    Move project to Gatsby. (#1)
    * :fire: remove(project): remove next project
    
    * :tada: starting gatsby project

--- components/ui/Heading.js
+++
@@ -1,8 +0,0 @@
-import tw, { styled } from 'twin.macro';
-
-const Heading = styled.h1(() => [
-    tw`text-4xl font-medium mb-2`,
-    `font-family: 'JetBrains Mono';`,
-]);
-
-export default Heading;
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import 'tailwindcss/dist/base.min.css';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
