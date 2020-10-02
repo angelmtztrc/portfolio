488ed6adb77bf548848217a85e4ec90f60965f11
Angel Martinez <angel.codes139@gmail.com>
2020-10-02 16:39:51 -0500

    :wrench: config(gatsby): adding custom fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `fonts`,
+        path: `${__dirname}/src/fonts`
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
