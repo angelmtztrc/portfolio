78b32dbe3768809291dcf87288b289942d47d42a
Angel Martinez <angel.codes139@gmail.com>
2020-10-02 16:34:09 -0500

    :wrench: config(tailwindcss): adding tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#1f1f1f'
+      },
+      fontFamily: {
+        display: ['"JetBrains Mono"', 'sans-serif'],
+        body: ['"Roboto Mono"', 'monospace']
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
