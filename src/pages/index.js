6d8ed8ed7dc183587765933feaaab5fcd936f381
Angel Martinez <angel.codes139@gmail.com>
2020-10-03 17:20:06 -0500

    ✨ feature(Index): Adding basic information

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,27 @@ import Layout from '../containers/Layout';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hello World!</h1>
+    <div className="py-6 lg:py-24 flex justify-center items-center">
+      <div class="grid grid-cols-2 gap-12 lg:gap-10">
+        <div class="col-span-2 lg:col-span-1 flex flex-col justify-center text-dark">
+          <h1 class="text-3xl lg:text-4xl font-bold mb-4">
+            Hi, I'm Angel Martinez!
+          </h1>
+          <p class="text-base lg:text-lg">
+            Also known as angel_codes, I am a developer with more than 4 years
+            of experience mainly working in both Front-end and Back-end
+            JavaScript environments. I currently live in Coah, Mexico.
+          </p>
+          <div></div>
+        </div>
+        <div class="col-span-2 lg:col-span-1">
+          {/* <img
+                class="w-full object-cover"
+                src="assets/images/home-image.svg"
+              /> */}
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
