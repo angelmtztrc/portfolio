52bfbd1efbfbd07aab69f82684aaf94c6618c4e6
Angel Martinez <angel.codes139@gmail.com>
2020-10-03 18:20:21 -0500

    ✨ feature(Navbar): Adding routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Navbar = () => {
   return (
     <header className="w-full bg-white">
       <div className="container mx-auto px-6 lg:px-0 py-4 lg:py-6 flex flex-wrap justify-between items-center">
-        <a className="p-4 bg-dark flex items-center">
+        <Link to="/" className="p-4 bg-dark flex items-center">
           <span className="text-white font-display leading-none">AM</span>
-        </a>
+        </Link>
         <button className="lg:hidden">
           <svg
             className="w-6 h-6"
@@ -27,11 +28,41 @@ const Navbar = () => {
           id="navigation"
           className="mt-5 lg:mt-0 hidden lg:block lg:w-auto lg:flex-grow-0 text-dark"
         >
-          <a className="mb-3 lg:mb-0 text-gray-900 font-bold">Home</a>
-          <a className="mb-3 lg:mb-0 text-gray-900 font-bold">About</a>
-          <a className="mb-3 lg:mb-0 text-gray-900 font-bold">Blog</a>
-          <a className="mb-3 lg:mb-0 text-gray-900 font-bold">Portfolio</a>
-          <a className="mb-3 lg:mb-0 text-gray-900 font-bold">Contact</a>
+          <Link
+            to="/"
+            className="mb-3 lg:mb-0 text-gray-900 font-bold border-b-2 border-transparent hover:border-dark transition-colors ease-in-out duration-300"
+            activeClassName="border-b-2 border-dark"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="mb-3 lg:mb-0 text-gray-900 font-bold border-b-2 border-transparent hover:border-dark transition-colors ease-in-out duration-300"
+            activeClassName="border-b-2 border-dark"
+          >
+            About
+          </Link>
+          <Link
+            to="/blog"
+            className="mb-3 lg:mb-0 text-gray-900 font-bold border-b-2 border-transparent hover:border-dark transition-colors ease-in-out duration-300"
+            activeClassName="border-b-2 border-dark"
+          >
+            Blog
+          </Link>
+          <Link
+            to="/portfolio"
+            className="mb-3 lg:mb-0 text-gray-900 font-bold border-b-2 border-transparent hover:border-dark transition-colors ease-in-out duration-300"
+            activeClassName="border-b-2 border-dark"
+          >
+            Portfolio
+          </Link>
+          <Link
+            to="/contact"
+            className="mb-3 lg:mb-0 text-gray-900 font-bold border-b-2 border-transparent hover:border-dark transition-colors ease-in-out duration-300"
+            activeClassName="border-b-2 border-dark"
+          >
+            Contact
+          </Link>
         </nav>
       </div>
     </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,19 @@ import Layout from '../containers/Layout';
 const IndexPage = () => (
   <Layout>
     <div className="py-6 flex justify-center items-center">
-      <div class="grid grid-cols-2 gap-12 lg:gap-10">
-        <div class="col-span-2 lg:col-span-1 flex flex-col justify-center text-dark">
-          <h1 class="text-3xl lg:text-4xl font-bold mb-4">
+      <div className="grid grid-cols-2 gap-12 lg:gap-10">
+        <div className="col-span-2 lg:col-span-1 flex flex-col justify-center text-dark">
+          <h1 className="text-3xl lg:text-4xl font-bold mb-4">
             Hi, I'm Angel Martinez!
           </h1>
-          <p class="text-base lg:text-lg">
+          <p className="text-base lg:text-lg">
             Also known as angel_codes, I am a developer with more than 4 years
             of experience mainly working in both Front-end and Back-end
             JavaScript environments. I currently live in Coah, Mexico.
           </p>
           <div></div>
         </div>
-        <div class="col-span-2 lg:col-span-1">
+        <div className="col-span-2 lg:col-span-1">
           <WelcomeIcon className="w-full object-cover" />
         </div>
       </div>
