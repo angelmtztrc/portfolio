f2d6472b9e3d3476eb725ff7b0832e58b9968e1d
Angel Martinez <angel.codes139@gmail.com>
2020-10-05 15:50:56 -0500

    ✨ feature(Tailwindcss): Purge production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
   },
-  purge: [],
+  purge: ['./src/components/*.js', './src/containers/*.js', './src/pages/*.js'],
   theme: {
     extend: {
       colors: {
