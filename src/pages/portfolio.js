1a9ac2f494c32e15d940d19aad4088d1b810e44e
Angel Martinez <angel.codes139@gmail.com>
2020-10-22 22:55:59 -0500

    ✨ feature(workingon): creating a component for no-ready pages

---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Containers
+import Layout from '../containers/Layout';
+// Components
+import WorkingOn from '../components/WorkingOn';
+
+const PortfolioPage = () => {
+  return (
+    <Layout>
+      <WorkingOn />
+    </Layout>
+  );
+};
+
+export default PortfolioPage;
