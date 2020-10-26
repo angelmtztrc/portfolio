12b88a50fe6653625afee662558a53ede758c6d3
Angel Martinez <65056245+angel-codes@users.noreply.github.com>
2020-10-26 15:04:07 -0600

    🐛 fix(navbar): adding link to home (#5)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,6 +35,7 @@ const Navbar = () => {
           } mt-5 lg:mt-0 lg:flex lg:flex-row lg:items-center lg:w-auto lg:flex-grow-0 text-dark-primary`}
         >
           <Link
+            to="/"
             className="lg:hover:text-dark-primary lg:hover:bg-transparent lg:hover:border-dark-primary px-3 py-2 hover:text-light-primary font-bold hover:bg-dark-primary transition-colors duration-300 ease-in-out lg:mb-0 lg:px-2 lg:py-1 lg:border-2 lg:border-transparent"
             activeClassName="text-light-primary lg:text-dark-primary bg-dark-primary lg:bg-transparent lg:border-2 lg:border-dark-primary active-border"
           >
