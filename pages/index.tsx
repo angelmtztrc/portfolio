eeb419fd4ac3cfd6b9ebaed26e7524342205c4f6
Angel Martinez <angel.martineztrc@gmail.com>
2021-02-04 21:31:00 -0600

    feat(pages): :sparkles:  removing articles and projects section for the moment

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,10 @@ import Layout from '../containers/Layout';
 
 // components
 import Welcome from '../components/home/Welcome';
-import Articles from '../components/home/Articles';
-import Projects from '../components/home/Projects';
 
 const IndexPage = () => (
   <Layout title={'Home'}>
     <Welcome />
-    <Articles />
-    <Projects />
   </Layout>
 );
 
