ac8ef6f67a6c277491844102b0d12f304d004ac4
Angel Martinez <angel.martineztrc@gmail.com>
2021-02-05 14:11:45 -0600

    feat(components): :sparkles:  new design for projectpreview

--- components/ProjectPreview.tsx
+++ components/ProjectPreview.tsx
@@ -1,39 +1,41 @@
+import Icon from '@mdi/react';
+import { mdiGithub, mdiOpenInNew } from '@mdi/js';
 const ProjectPreview = () => {
   return (
-    <article>
-      <a
-        href="#"
-        className="flex flex-col items-center justify-center mx-auto max-w-sm"
-      >
-        <div className="w-full">
-          <img
-            className="w-full h-56"
-            src="https://images.unsplash.com/photo-1557599443-2071a2df9c19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1382&q=80"
-          />
-        </div>
-        <div className="bg-primary-300 -mt-10 p-6 w-11/12 shadow-lg">
-          <h3 className="text-primary-600 mt-2 text-xl font-bold">
-            NextJS with TypeScript
-          </h3>
-          <ul className="flex items-center mt-1 space-x-4">
-            <li className="text-gray-500 text-sm">Tutorial</li>
-            <li className="text-gray-500 text-sm">Tips</li>
-          </ul>
-          <p className="mt-4 text-gray-500 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
-            minima autem, aut natus repellendus quidem officia ea quos qui
-            veniam?
-          </p>
-          <div className="flex mt-4 space-x-2">
-            <button className="text-primary-600 hover:bg-primary-600 border-primary-600 px-4 py-2 hover:text-white text-sm font-medium border-2 transition-colors duration-300 ease-in">
-              Preview
-            </button>
-            <button className="text-primary-600 hover:bg-primary-600 border-primary-600 px-4 py-2 hover:text-white text-sm font-medium border-2 transition-colors duration-300 ease-in">
-              Code
-            </button>
+    <article className="grid grid-cols-2">
+      <div className="col-span-1 ml-16">
+        <img
+          className="w-full object-cover"
+          src="https://images.unsplash.com/photo-1557599443-2071a2df9c19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1382&q=80"
+        />
+      </div>
+      <div className="col-span-1 -ml-16 pr-16 py-6">
+        <div className="p-6 w-full h-full bg-gray-100 border border-blue-200 shadow-lg">
+          <div className="flex items-center">
+            <p className="text-gray-900 font-sans text-xl font-bold">
+              MERNTask Project
+            </p>
+            <div className="flex items-center ml-4 space-x-2">
+              <a href="#">
+                <Icon className="w-5 h-5" path={mdiGithub} />
+              </a>
+              <a href="#">
+                <Icon className="w-5 h-5" path={mdiOpenInNew} />
+              </a>
+            </div>
           </div>
+          <p className="mt-4 text-gray-600">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            quo culpa magni molestias molestiae architecto quaerat provident
+            explicabo perferendis rerum labore laudantium atque, fugiat autem
+            consectetur. In atque expedita quos?
+          </p>
+          <ul className="flex items-center mt-3 text-blue-500 text-sm space-x-4">
+            <li>JavaScript</li>
+            <li>NodeJS</li>
+          </ul>
         </div>
-      </a>
+      </div>
     </article>
   );
 };
--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,3 +1,4 @@
+import ProjectPreview from '../ProjectPreview';
 import SubHeading from '../SubHeading';
 
 const Projects = () => {
@@ -5,7 +6,9 @@ const Projects = () => {
     <section className="mt-32">
       <div className="container mx-auto">
         <SubHeading number="03" text="Projects I have built" />
-        {/* TODO: Design for a featured project */}
+        <div className="flex flex-col justify-center">
+          <ProjectPreview />
+        </div>
       </div>
     </section>
   );
