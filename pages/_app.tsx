42776f3a267c864c09a993f835d51e8dcdad77f4
Angel Martinez <angel.martineztrc@gmail.com>
2021-02-04 19:47:58 -0600

    feat(pages): :fire:  removing nightwind from the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider
-      attribute="class"
-      storageKey="nightwind-mode"
-      defaultTheme="light"
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
