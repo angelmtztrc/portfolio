87a5e580f2bf02928247abe7e31498cfa4969291
Angel Martinez <angel.martineztrc@gmail.com>
2021-02-24 22:47:22 -0600

    feat(AboutMe): :sparkles:  responsive design

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -3,14 +3,14 @@ export type AboutMeProps = {};
 const AboutMe = () => {
   return (
     <div className="grid gap-10 grid-cols-2 mt-24">
-      <div className="w-full">
+      <div className="col-span-2 w-full lg:col-span-1">
         <img
           className="w-full object-fill"
           src="https://images.unsplash.com/photo-1592685258314-73731368acd6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80"
           alt="some picture"
         />
       </div>
-      <div className="font-sans">
+      <div className="col-span-2 font-sans lg:col-span-1">
         <h3 className="text-gray-900 text-xl">
           About <span className="text-red-500 font-bold">Me</span>
         </h3>
