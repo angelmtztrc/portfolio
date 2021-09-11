07ee1edbcdce73c4f99724009dd2f1f74ee1115e
Angel Martinez <angel.martineztrc@gmail.com>
2021-09-11 14:05:03 -0500

    feat(tailwindcss):  add jetbrains mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['JetBrains Mono', 'monospace']
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
