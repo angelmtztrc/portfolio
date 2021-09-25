b7b9e216b35545bf859a10c193df38e147597f48
Angel Martinez <angel.martineztrc@gmail.com>
2021-09-24 22:39:40 -0500

    chore(tailwindcss):  add purge values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/containers/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
