1d20d3b207a723d58afa6f18c165053be9d897cd
Angel Martinez <angel.martineztrc@gmail.com>
2021-09-25 14:28:41 -0500

    feat(libs/request):  add a request function for make request to the graphql server

---
+++ src/libs/request.ts
@@ -0,0 +1,31 @@
+import { GraphQLClient } from 'graphql-request';
+import { Variables } from 'graphql-request/dist/types';
+
+/**
+ * Send request to a GraphQL API
+ * @param {{query: string, variables?: Variables, preview?: boolean}} RequestProps - Object to construct the request
+ * @returns Promise<unknown>
+ */
+export const request = ({
+  query,
+  variables,
+  preview
+}: RequestProps): Promise<unknown> => {
+  const endpoint = preview
+    ? `https://graphql.datocms.com/preview`
+    : `https://graphql.datocms.com/`;
+
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`
+    }
+  });
+
+  return client.request(query, variables);
+};
+
+type RequestProps = {
+  query: string;
+  variables?: Variables;
+  preview?: boolean;
+};
