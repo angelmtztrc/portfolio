4fa10474afc97142fa83d9133f0287cbe214a71b
Angel Martinez <angel.martineztrc@gmail.com>
2021-09-25 00:18:08 -0500

    fix(RepositoriesSection):  change flex to grid

--- src/components/repositories-section.tsx
+++ src/components/repositories-section.tsx
@@ -5,8 +5,48 @@ export const RepositoriesSection = ({}: RepositoriesSectionProps) => {
     <Tab.Panel as="section">
       {/* Title and description here */}
 
-      <div className="flex flex-wrap">
-        <article className="w-full lg:w-1/2">
+      <div className="grid gap-6 grid-cols-2">
+        <article className="col-span-2 lg:col-span-1">
+          <div className="p-6 bg-raisin-black bg-opacity-50 rounded">
+            <img
+              className="mb-6 w-full h-48 rounded object-cover object-center"
+              src="https://dummyimage.com/720x400"
+              alt="content"
+            />
+            <h3 className="text-medium-state-blue font-body text-xs font-semibold tracking-widest uppercase">
+              TypeScript
+            </h3>
+            <h2 className="font-display mb-4 text-white text-lg font-medium">
+              Passgen CLI
+            </h2>
+            <p className="text-cadet-grey font-body text-base font-light leading-relaxed">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Perferendis sequi voluptate est voluptates nisi natus beatae.
+              Corporis numquam natus deserunt.
+            </p>
+          </div>
+        </article>
+        <article className="col-span-2 lg:col-span-1">
+          <div className="p-6 bg-raisin-black bg-opacity-50 rounded">
+            <img
+              className="mb-6 w-full h-48 rounded object-cover object-center"
+              src="https://dummyimage.com/720x400"
+              alt="content"
+            />
+            <h3 className="text-medium-state-blue font-body text-xs font-semibold tracking-widest uppercase">
+              TypeScript
+            </h3>
+            <h2 className="font-display mb-4 text-white text-lg font-medium">
+              Passgen CLI
+            </h2>
+            <p className="text-cadet-grey font-body text-base font-light leading-relaxed">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Perferendis sequi voluptate est voluptates nisi natus beatae.
+              Corporis numquam natus deserunt.
+            </p>
+          </div>
+        </article>
+        <article className="col-span-2 lg:col-span-1">
           <div className="p-6 bg-raisin-black bg-opacity-50 rounded">
             <img
               className="mb-6 w-full h-48 rounded object-cover object-center"
