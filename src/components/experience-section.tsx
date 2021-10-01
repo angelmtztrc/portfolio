9b0bc39349d9e1e34e691854320d6c8a124a526d
Angel Martinez <angel.martineztrc@gmail.com>
2021-09-30 22:17:58 -0500

    feat(education/experience):  add work in progress title

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -3,7 +3,15 @@ import { Tab } from '@headlessui/react';
 export const ExperienceSection = ({}: ExperienceSectionProps) => {
   return (
     <Tab.Panel as="section">
-      <p className="text-white">Experience section</p>
+      <div className="flex flex-col items-center justify-center py-16">
+        <h2 className="text-white font-display text-3xl font-bold capitalize">
+          Work In Progress
+        </h2>
+        <p className="mt-10 w-1/2 text-center text-cadet-grey font-body font-light">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id vitae
+          quasi fuga possimus ab voluptas illo odit totam eaque sint!
+        </p>
+      </div>
     </Tab.Panel>
   );
 };
