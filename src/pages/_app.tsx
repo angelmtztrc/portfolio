9f8a91fb0c816b4e6c11808a8be2c92ac169f5b1
Angel Martinez <angel.martineztrc@gmail.com>
2022-07-05 19:59:23 -0500

    chore:  cleaning the project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const HomePage: NextPage<HomePageProps> = () => {
+  return <div>Hello, World!</div>;
+};
+
+type HomePageProps = {};
+
+export default HomePage;
