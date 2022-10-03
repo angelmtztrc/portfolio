1689fb643b4f7e95b5f867a344adff83c2ca7c60
Angel Martinez <angel.martineztrc@gmail.com>
2022-10-03 18:25:49 -0500

    feat:  add colors and fonts for tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-900">
-      <h1 className="text-2xl font-bold text-white">Hello, World!</h1>
+    <div className="flex min-h-screen items-center justify-center bg-darken-900">
+      <h1 className="font-display text-2xl font-bold text-white">Hello, World!</h1>
     </div>
   );
 };
