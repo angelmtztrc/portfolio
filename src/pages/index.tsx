204bc2a996cff634b05d4df80ee0133dee242282
Angel Martinez <angel.martineztrc@gmail.com>
2022-10-25 15:22:09 -0500

    chore:  wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next';
+import { BriefcaseIcon } from '@heroicons/react/24/solid';
 
-import { SkillsList, SocialMediaList } from '@atoms';
+import { ProjectItem, SkillsList, SocialMediaList } from '@atoms';
 import { Biography, Navbar } from '@molecules';
 
 const HomePage: NextPage = () => {
   return (
     <div className="min-h-screen bg-darken-900">
-      <div className="h-48 bg-primary-500" />
-      <main className="container mx-auto  grid grid-cols-1 gap-4 px-4 lg:grid-cols-12">
+      <div className="h-52 bg-primary-500" />
+      <main className="container mx-auto grid grid-cols-1 gap-8 px-4 lg:grid-cols-12">
         <div className="-mt-12 flex flex-col gap-4 lg:col-span-3">
           <Biography />
           <SocialMediaList />
@@ -16,23 +17,21 @@ const HomePage: NextPage = () => {
         <div className="flex flex-col gap-4 lg:col-span-9 lg:-mt-12">
           <Navbar />
           <section className="rounded-lg border border-dark-900 bg-darken-900 p-5">
-            <div className="flex items-center gap-2">
-              <div className="h-8 w-8 rounded-full bg-secondary-500" />
+            <div className="flex items-center gap-4">
+              <div className="rounded-full bg-purple-500 bg-opacity-10 p-2">
+                <BriefcaseIcon className="h-8 w-8 rounded-full text-primary-400" />
+              </div>
               <h2 className="font-display text-2xl font-bold text-white">Projects</h2>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 lg:grid-cols-3">
               {[1, 2, 3].map(i => (
-                <article
+                <ProjectItem
                   key={i}
-                  className="cursor-pointer rounded-lg border border-dark-900 p-4 transition-colors duration-300 ease-in hover:bg-dark-900"
-                >
-                  <figure className="aspect-video rounded-lg bg-secondary-500" />
-                  <h3 className="mt-4 font-display text-xl font-bold text-white">Passgen - Deno</h3>
-                  <p className="mt-2 font-body text-base font-light leading-relaxed text-greyish-900">
-                    A command-line tool built with Deno and TypeScript that allows you to generate
-                    random and secure passwords.
-                  </p>
-                </article>
+                  title="Passgen - Deno"
+                  description="A command-line tool built with Deno and TypeScript that allows you to generate random
+            and secure passwords."
+                  github="https://angelmtz.dev"
+                />
               ))}
             </div>
           </section>
---
+++ src/utils/menu.util.ts
@@ -0,0 +1,22 @@
+export const menu = [
+  {
+    title: 'projects',
+    path: '/'
+  },
+  {
+    title: 'blog',
+    path: '/blog'
+  },
+  {
+    title: 'experience',
+    path: '/experience'
+  },
+  {
+    title: 'education',
+    path: '/education'
+  },
+  {
+    title: 'contact',
+    path: '/contact'
+  }
+];
