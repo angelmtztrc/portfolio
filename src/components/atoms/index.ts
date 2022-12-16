a3be3cd8b396b58085d06e3d5a01a865ec114e12
Angel Martinez <angel.martineztrc@gmail.com>
2022-12-16 11:44:03 -0600

    feat:  add page heading

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as MenuItem } from './MenuItem';
+export { default as PageHeading } from './PageHeading';
 export { default as ProjectItem } from './ProjectItem';
 export { default as AbilitiesList } from './AbilitiesList';
 export { default as SocialMediaList } from './SocialMediaList';
