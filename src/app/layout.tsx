542b6c724b90f5f50be7edbdb33d2c785f050e92
Angel Martinez <angel.martineztrc@gmail.com>
2022-12-17 13:24:47 -0600

    fix:  add meta viewport tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ const EntryLayout = ({ children }: EntryLayoutProps) => {
     <html lang="en" className={fonts}>
       <head>
         <title>Angel Martinez | Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <div className="min-h-screen bg-darken-900">
