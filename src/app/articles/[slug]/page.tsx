03feb40a94734e3ae5d018fff4be5dc4f0da4cf2
Angel Martinez <angel.martineztrc@gmail.com>
2022-12-17 12:35:14 -0600

    feat:  fill data of articles

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,59 +1,51 @@
+import { use } from 'react';
+import Image from 'next/image';
+import MarkdownRender from 'react-markdown';
 import { ArrowSmallLeftIcon } from '@heroicons/react/24/solid';
 
 import { Button } from '@atoms';
+import { ArticlesService } from '@services';
+import Link from 'next/link';
 
-const ArticlePage = () => {
+const fetcher = async (slug: string) => {
+  const data = await ArticlesService.findOneBySlug(slug);
+
+  return data.article;
+};
+
+const ArticlePage = ({ params }: { params: { slug: string } }) => {
+  const data = use(fetcher(params.slug));
+
   return (
     <article className="rounded-lg border border-dark-900 bg-darken-900 px-12 py-8">
       <header>
         <div>
-          {/* TODO: LEFT ARROW ICON */}
-          <Button className="flex items-center space-x-2 px-6 py-3">
-            <ArrowSmallLeftIcon className="h-4 w-4 text-white" />
-            <span>Back</span>
-          </Button>
+          <Link href="/articles">
+            <Button className="flex items-center space-x-2 px-6 py-3">
+              <ArrowSmallLeftIcon className="h-4 w-4 text-white" />
+              <span>Back</span>
+            </Button>
+          </Link>
         </div>
-        <figure className="mt-4 aspect-[16/8] w-full overflow-hidden rounded-lg">
-          <div className="h-full w-full bg-secondary-500" />
+        <figure className="relative mt-4 aspect-[16/8] w-full overflow-hidden rounded-lg">
+          <Image src={data.picture.url} alt={data.title} className="object-cover" fill />
         </figure>
         <div className="mt-6 px-12">
-          <h1 className="font-display text-4xl font-bold text-white">
-            Nest.js - Introduction (Part I)
-          </h1>
-          <ul className="mt-2 flex items-center space-x-2">
-            <li className="font-body font-light text-gray-800">
-              <span className="text-red-500">#</span>typescript
-            </li>
-            <li className="font-body font-light text-gray-800">
-              <span className="text-purple-500">#</span>backend
-            </li>
-            <li className="font-body font-light text-gray-800">
-              <span className="text-green-500">#</span>nest
-            </li>
-            <li className="font-body font-light text-gray-800">
-              <span className="text-blue-500">#</span>prisma
-            </li>
-          </ul>
+          <h1 className="font-display text-4xl font-bold text-white">{data.title}</h1>
+          <div>
+            <ul className="mt-2 flex items-center space-x-2">
+              {data.tags.split(',').map(tag => (
+                <li key={tag} className="font-body font-light text-gray-800">
+                  <span className="text-red-500">#</span>
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </header>
-      <div className="mt-12 px-12">
-        {/* TODO: MARKDOWN RENDER */}
-        <p className="font-body font-light text-greyish-800">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque hic quo dolore unde
-          consectetur expedita perspiciatis natus rem perferendis facere.
-        </p>
-        <p className="font-body font-light text-greyish-800">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque hic quo dolore unde
-          consectetur expedita perspiciatis natus rem perferendis facere.
-        </p>
-        <p className="font-body font-light text-greyish-800">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque hic quo dolore unde
-          consectetur expedita perspiciatis natus rem perferendis facere.
-        </p>
-        <p className="font-body font-light text-greyish-800">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque hic quo dolore unde
-          consectetur expedita perspiciatis natus rem perferendis facere.
-        </p>
+      <div className="prose prose-lg prose-invert mt-12 w-full max-w-full px-12">
+        <MarkdownRender>{data.content}</MarkdownRender>
       </div>
     </article>
   );
