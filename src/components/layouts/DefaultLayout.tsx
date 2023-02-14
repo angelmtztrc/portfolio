f614763ce8c9b116a3b13605b939b338ff1a7219
Angel Martinez <angel.martineztrc@gmail.com>
2023-02-14 14:42:41 -0600

    feat:  add analytics

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { hotjar } from 'react-hotjar';
+import { Analytics } from '@vercel/analytics/react';
 
 type DefaultLayoutProps = {
   children: React.ReactNode;
@@ -12,7 +13,12 @@ const DefaultLayout = ({ children }: DefaultLayoutProps) => {
     hotjar.initialize(3367181, 6);
   }, []);
 
-  return <div className="min-h-screen bg-darken-900">{children}</div>;
+  return (
+    <div className="min-h-screen bg-darken-900">
+      {children}
+      <Analytics />
+    </div>
+  );
 };
 
 export default DefaultLayout;
